6a149102e56f73a80a5d972e2479b89278674c22
ksaper <sa_1@Bester.local>
2017-05-15 14:12:11 -0700

    update

--- unit.py
+++ unit.py
@@ -8,9 +8,10 @@ unit = ResourceAvailability()
 # for user in user_list:
 #     print user.Name
 
-target = ['JPN_1', 'JPN_2']
+target = ['ct-z9100-1', 'dv-ngos-s4148u-2', 'ngos-s6000-4', 'dv-fedgov-m1000e-1', 'st-sjc-s4048-1', 'st-sjc-s4048t-1',
+          'dv-sjc-mcast-nav-102', 'dv-s4810-7']
 start = '10/05/2017 18:00'
-end = '11/05/2017 20:00'
+end = '17/05/2017 18:00'
 # cap = unit._get_reservations(target)
 # print cap
 # print unit._convert_to_ISO8601('02/14/2017 23:55')
